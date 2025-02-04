0857bd80f93c5bf2a8886ef13f444ea8cbe788ef
jourdainkevin <jourdain.kevin33@gmail.com>
2025-02-04 13:02:01 +0100

    [ADD] : Flyweight Pattern

---
+++ src/FlyweightPattern/Order.java
@@ -0,0 +1,18 @@
+package FlyweightPattern;
+
+public class Order {
+    private String name;
+
+    public Order(String name){
+        this.name = name;
+
+        System.out.println("Creating " + name + " object");
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        System.out.println("Done creating " + name + " object");
+    }
+
+}
---
+++ src/FlyweightPattern/challenge/FlowerFactory.java
@@ -0,0 +1,15 @@
+package FlyweightPattern.challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlowerFactory {
+
+    private static final Map<String, Flower> flowerCache = new HashMap<>();
+
+    public static Flower createFlower(String name){
+
+        return flowerCache.computeIfAbsent(name, Flower::new);
+    }
+
+}
